01c6052ef692d93d3394e42e940a1d26d242bab1
Matthias Goldhoorn <matthias.goldhoorn@dfki.de>
2015-03-09 08:49:43 +0100

    Code cleanup (minmal)

---
+++ cobject.rb
@@ -0,0 +1,204 @@
+
+
+class Requirement
+
+    attr_writer :enabled
+    attr_reader :name
+
+    def disabled?
+        @disabled
+    end
+
+    def initialize(dep, disabled= false)
+        @disabled=disabled 
+        @name = dep
+    end
+end
+
+
+class BaseObject
+    attr_accessor :requires
+    attr_accessor :depends
+    attr_accessor :fullfills
+    attr_writer :running
+    
+    def initialize
+        @requires = []
+        @depends = []
+        @fullfills = []
+        @running = false
+    end
+    
+    def input_ports
+        []
+    end
+    
+    def output_ports
+        []
+    end
+    
+    def is_running?
+        @running
+    end
+    
+    def addDependancy(name, disabled=false)
+        @requires<< Requirement.new(name,disabled)
+    end
+    
+    def each_fullfillment
+        return enum_for(:each_fullfillment) unless block_given?
+        fullfills.each do |value|
+            yield(value.name.escape)
+        end
+    end
+
+end
+
+class RootObject < BaseObject
+
+    def initialize
+        super
+        @requires << Requirement.new("root")
+        @depends = [Requirement.new("root")]
+        @running = true
+    end
+   
+    def type
+        "task"
+    end
+    
+    def metatype 
+        "instance_req"
+    end
+
+    def name
+        "root"
+    end
+end
+
+class CObject < BaseObject
+    attr_accessor :task
+#    attr_accessor :type
+
+    #def initialize(task,type="")
+    def initialize(task)
+        super()
+        @task= task 
+    end
+
+
+    def addDependancy(name, disabled=false)
+        @depends << Requirement.new(name,disabled)
+    end
+
+
+    def metatype
+        if task.kind_of? Syskit::Models::OutputPort 
+#        elsif task.instance_of? OroGen::Spec::OutputPort
+            "output_port"
+        elsif task.kind_of? Syskit::Models::InputPort 
+        #elsif task.instance_of? OroGen::Spec::InputPort
+            "input_port"
+        elsif task.kind_of? Syskit::Models::Composition
+            "instance_req"
+        elsif task.kind_of? Syskit::Models::DataServiceModel
+            "instance_req"
+        elsif task < Syskit::TaskContext
+        #if task.instance_of? OroGen::Spec::TaskContext 
+            "instance_req"
+        else
+            binding.pry
+            "Unknown"
+        end
+
+    end
+
+    def type
+        if task.kind_of? Syskit::Models::OutputPort 
+        #elsif task.instance_of? OroGen::Spec::OutputPort
+            nil
+        elsif task.kind_of? Syskit::Models::InputPort 
+        #elsif task.instance_of? OroGen::Spec::InputPort
+            nil
+        elsif task.kind_of? Syskit::Models::Composition
+            "composition"
+        elsif task.kind_of? Syskit::Models::DataServiceModel
+            "data-service"
+        elsif task < Syskit::TaskContext
+        #if task.instance_of? OroGen::Spec::TaskContext 
+            "task"
+        else
+            binding.pry
+            "Unknown"
+        end
+    end
+    
+    def input_ports
+        begin
+            if task < Syskit::TaskContext
+            #if task.instance_of? OroGen::Spec::TaskContext
+                a = Hash.new
+                task.input_ports.each do |p|
+                    a[p[1].name] = p[1]
+                end
+                return a.values
+            end
+        rescue Exception => e
+            ##Nothing
+        end
+        
+        return []
+    end
+    
+    def output_ports
+        begin
+            if task < Syskit::TaskContext
+    #        if task.instance_of? OroGen::Spec::TaskContext
+                a = Hash.new
+                task.output_ports.each do |p|
+                    a[p[1].name] = p[1]
+                end
+                return a.values
+            end
+        rescue Exception => e
+            #nothing
+        end
+        return [] 
+    end
+
+    def name
+        if 
+#        task.instance_of? OroGen::Spec::TaskContext or 
+        task.kind_of? Syskit::Models::DataServiceModel or 
+        task.kind_of? Syskit::Models::Composition 
+            task.name.gsub(' ','').escape
+        elsif 
+#            task.instance_of? OroGen::Spec::OutputPort
+#            task.instance_of? OroGen::Spec::InputPort
+            task.instance_of? Syskit::Models::InputPort or 
+            task.instance_of? Syskit::Models::OutputPort
+            "#{task.component_model.name.to_s.gsub(' ','')}.#{task.name}".escape
+        elsif
+        task < Syskit::TaskContext 
+            task.name.gsub(' ','').escape
+        else
+            binding.pry
+        end
+    end
+end
+
+class CObjects < Array
+
+    def [] (op)
+        if(op.instance_of? Fixnum)
+            super[op]
+        elsif (op.instance_of? String)
+            self.each do |v|
+                if v.name == op.escape
+                    return v
+                end
+            end
+        end
+        nil
+    end
+end
--- dump-tasks.rb
+++ dump-tasks.rb
@@ -2,7 +2,7 @@ require 'orocos'
 require 'erubis'
 require 'roby'
 require 'syskit'
-
+require './cobject'
 
 
 class Binding
@@ -20,231 +20,24 @@ class String
         self.gsub(pattern){|match| "_"}
     end
 end
-class Requirement
-
-    attr_writer :enabled
-    attr_reader :name
-
-    def disabled?
-        @disabled
-    end
-
-    def initialize(dep, disabled= false)
-        @disabled=disabled 
-        @name = dep
-    end
-end
 
 
-class RootObject
-    attr_accessor :requires
-
-    def initialize
-        @requires = []
-        @requires << Requirement.new("root")
-    end
-   
-    def is_running?
-        true
-    end
-
-    def addDependancy(name, disabled=false)
-        @requires<< Requirement.new(name,disabled)
-    end
-
-    def type
-        "task"
-    end
-    
-    def metatype 
-        "instance_req"
-    end
-
-    def name
-        "root"
-    end
-   
-    def input_ports
-        []
-    end
-    
-    def output_ports
-        []
-    end
-
-    def each_fullfillment
-        [].each
-    end
-
-    def depends 
-        [Requirement.new("root")]
-        #["root"]
-    end
-
-end
-
-class CObject
-    attr_accessor :task
-    attr_accessor :type
-    attr_accessor :depends
-    attr_accessor :requires
-    attr_writer :running
-    attr_accessor :fullfills
-
-    def initialize(task,type="")
-        @depends = []
-        @requires= []
-        @task= task 
-        @type = type
-        @running = false
-        @fullfills = []
-    end
-
-    def is_running?
-        @running
-    end
-
-    def addDependancy(name, disabled=false)
-        @depends << Requirement.new(name,disabled)
-    end
-
-    def each_fullfillment
-        return enum_for(:each_fullfillment) unless block_given?
-        fullfills.each do |value|
-            yield(value.name.escape)
-        end
-    end
-
-    def metatype
-        if task.kind_of? Syskit::Models::OutputPort 
-#        elsif task.instance_of? OroGen::Spec::OutputPort
-            "output_port"
-        elsif task.kind_of? Syskit::Models::InputPort 
-        #elsif task.instance_of? OroGen::Spec::InputPort
-            "input_port"
-        elsif task.kind_of? Syskit::Models::Composition
-            "instance_req"
-        elsif task.kind_of? Syskit::Models::DataServiceModel
-            "instance_req"
-        elsif task < Syskit::TaskContext
-        #if task.instance_of? OroGen::Spec::TaskContext 
-            "instance_req"
-        else
-            binding.pry
-            "Unknown"
-        end
-
-    end
-
-    def type
-        if task.kind_of? Syskit::Models::OutputPort 
-        #elsif task.instance_of? OroGen::Spec::OutputPort
-            nil
-        elsif task.kind_of? Syskit::Models::InputPort 
-        #elsif task.instance_of? OroGen::Spec::InputPort
-            nil
-        elsif task.kind_of? Syskit::Models::Composition
-            "composition"
-        elsif task.kind_of? Syskit::Models::DataServiceModel
-            "data-service"
-        elsif task < Syskit::TaskContext
-        #if task.instance_of? OroGen::Spec::TaskContext 
-            "task"
-        else
-            binding.pry
-            "Unknown"
-        end
-    end
-    
-    def input_ports
-        begin
-            if task < Syskit::TaskContext
-            #if task.instance_of? OroGen::Spec::TaskContext
-                a = Hash.new
-                task.input_ports.each do |p|
-                    a[p[1].name] = p[1]
-                end
-                return a.values
-            end
-        rescue Exception => e
-            ##Nothing
-        end
-        
-        return []
-    end
-    
-    def output_ports
-        begin
-            if task < Syskit::TaskContext
-    #        if task.instance_of? OroGen::Spec::TaskContext
-                a = Hash.new
-                task.output_ports.each do |p|
-                    a[p[1].name] = p[1]
-                end
-                return a.values
-            end
-        rescue Exception => e
-            #nothing
-        end
-        return [] 
-    end
-
-    def name
-        if 
-#        task.instance_of? OroGen::Spec::TaskContext or 
-        task.kind_of? Syskit::Models::DataServiceModel or 
-        task.kind_of? Syskit::Models::Composition 
-            task.name.gsub(' ','').escape
-        elsif 
-#            task.instance_of? OroGen::Spec::OutputPort
-#            task.instance_of? OroGen::Spec::InputPort
-            task.instance_of? Syskit::Models::InputPort or 
-            task.instance_of? Syskit::Models::OutputPort
-            "#{task.component_model.name.to_s.gsub(' ','')}.#{task.name}".escape
-        elsif
-        task < Syskit::TaskContext 
-            task.name.gsub(' ','').escape
-        else
-            binding.pry
-        end
-    end
-end
 
 
-    Roby.app.robot 'avalon'
-    Roby.app.using_plugins 'syskit'
-    Syskit.conf.only_load_models = true
-    Syskit.conf.disables_local_process_server = true
-    Roby.app.single = true
-    Syskit.conf.disable_logging
-    Syskit.conf.disable_conf_logging
-    
-    Roby.app.setup
+
+Roby.app.robot 'avalon'
+Roby.app.using_plugins 'syskit'
+Syskit.conf.only_load_models = true
+Syskit.conf.disables_local_process_server = true
+Roby.app.single = true
+Syskit.conf.disable_logging
+Syskit.conf.disable_conf_logging
+
+Roby.app.setup
 #Orocos.initialize
 
-class Objects < Array
 
-    def [] (op)
-        if(op.instance_of? Fixnum)
-            super[op]
-        elsif (op.instance_of? String)
-            self.each do |v|
-                if v.name == op.escape
-                    return v
-                end
-            end
-        end
-        nil
-    end
-end
-
-objects = Objects.new
-predicates = Array.new
-initial = Array.new
-goals = Array.new
-actions = Array.new
-
-tasks = Array.new
+    objects = CObjects.new
 
     objects << RootObject.new
 
@@ -274,11 +67,6 @@ tasks = Array.new
     Syskit::Composition.each_submodel do |cmp|
         o = CObject.new(cmp)
         cmp.each_child do |child_name,child|
-#            name = child[0]
-#            binding.pry
-#            child = child[1] # composition child
-#            objects << CObject.new(child)
-            #o.addDependancy(name.escape)
             o.addDependancy(child.model.name.gsub(" ","").escape)
         end
         
@@ -308,12 +96,6 @@ tasks = Array.new
     #test for shutdown
     objects['DepthReader::Task'].running = true
 
-#    STDOUT.puts "Size: #{objects['DepthReader::Task'].each_fullfillment.to_a.size}"
-#    objects['DepthReader::Task'].each_fullfillment do |v|
-#        binding.pry
-#    end
 
 
-
-
-binding.render("problem.pddl.template","problem.pddl")
+    binding.render("problem.pddl.template","problem.pddl")
